91bc62e78f59c59d1395a30446e187b1d6658fdf
nupniichan <bao96065@gmail.com>
2024-07-22 00:01:06 +0700

    Create profile page

---
+++ links.js
@@ -0,0 +1,27 @@
+export const links = [ 
+	{ 
+		name: "Facebook", 
+		link: "https://www.facebook.com/nupniichan/", 
+		image: "Image/facebook.png", 
+	},
+	{ 
+		name: "Github", 
+		link: "https://github.com/nupniichan", 
+		image: "Image/github.png", 
+	},  
+	{ 
+		name: "Youtube", 
+		link: "https://www.youtube.com/@nupniichan", 
+		image: "Image/youtube.png", 
+	}, 
+	{ 
+		name: "Discord", 
+		link: "https://discord.gg/EJcRDCQnPh", 
+		image: "Image/discord.png", 
+	}, 
+	{ 
+		name: "Anilist", 
+		link: "https://anilist.co/user/nupniichan/", 
+		image: "Image/Anilist.png", 
+	}, 
+];
