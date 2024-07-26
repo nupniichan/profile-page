f7983e8e70abb61b077dd67863d2cf4b806e8817
nupniichan <bao96065@gmail.com>
2024-07-26 19:27:43 +0700

    feat: Cập nhật tính năng mới

--- main.js
+++ main.js
@@ -1,25 +1,34 @@
-import { links } from "./links.js"; 
+import { links } from "./links.js";
 
-const linkContainer = document.getElementById("links"); 
+const linkContainer = document.getElementById("links");
 
-function addLink(name, link, image) { 
-	return ` 
-<a href="${link}" class="link" target="blank"> 
-	<img src="${image}"/> 
-	<span>${name} </span> 
-	<img class="linkIcon" src=${image} alt=""/> 
+function addLink(name, link, image) {
+    return `
+<a href="${link}" class="link" target="_blank"> 
+    <img src="${image}" alt=""/> 
+    <span>${name}</span> 
+    <img class="linkIcon" src="${image}" alt=""/> 
 </a> 
-`; 
-} 
+`;
+}
 
-let allLinks = ""; 
+let allLinks = "";
 
-links.forEach((ele) => { 
-	let link = ele.link; 
-	let name = ele.name; 
-	let image = ele.image; 
-
-	allLinks += addLink(name, link, image); 
-}); 
+links.forEach((ele) => {
+    allLinks += addLink(ele.name, ele.link, ele.image);
+});
 
 linkContainer.innerHTML = allLinks;
+
+const openModal = document.getElementById("openModal");
+const modal = document.getElementById("myModal");
+
+openModal.onclick = function () {
+    modal.style.display = "block";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
